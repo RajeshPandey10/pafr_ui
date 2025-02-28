40ef8b05d17e65123f6caef75be6809825e0252f
RajeshPandey10 <imrajesh2005@gmail.com>
2025-02-28 21:29:35 +0545

    ui updated

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
+import Loader from "./components/Loader.jsx";
+import BackToTop from "./components/BackToTop";
+import Footer from "./components/Footer";
+import Home from "./pages/home/Home.jsx";
+import Navbar from "./components/Navbar";
+import About from "./pages/about/About";
+import Features from "./pages/features/Features";
+import HowItWork from "./pages/howItWork/HowItWork";
+import Faq from "./components/Faq";
+import Contact from "./pages/message/Contact";
+
+
+const AppContent = () => {
+  const [loading, setLoading] = useState(true);
+  const location = useLocation();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div className="min-h-screen bg-white">
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="relative">
+          {/* Conditionally render Navbar if not on admin path */}
+          {location.pathname !== "/admin" && <Navbar />}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/features" element={<Features />} />
+            <Route path="/how-to-use" element={<HowItWork />} />
+            <Route path="/faq" element={<Faq />} />
+            <Route path="/contact" element={<Contact />} />
+           
+          </Routes>
+         
+              <BackToTop />
+              <Footer />
+            
+        </div>
+      )}
+    </div>
+  );
+};
+
+const App = () => (
+  <Router>
+    <AppContent />
+  </Router>
+);
+
+export default App;
